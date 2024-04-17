85322fc927b25003ab1e14f2c2f6498bb1237f5d
casuallyblue <sierra@casuallyblue.dev>
2024-04-16 20:53:54 -0500

    Get gcc working

--- csrc/functions.c
+++ csrc/functions.c
@@ -2,18 +2,13 @@
 #include<dirent.h>
 #include<sys/wait.h>
 
-void run_git(char* url, char* path) {
+int path_exists(char* path) {
 	DIR* dir = opendir(path);
 	if(dir) {
 		closedir(dir);
-		return;
+		return 1;
 	}
 
-	if(fork() == 0) {
-	    char* args[] = { "git", "clone", url, path, 0 };
-	    execvp("git", args);
-	} else {
-	    int pid;
-	    wait(&pid);
-	}
+	return 0;
 }
+
